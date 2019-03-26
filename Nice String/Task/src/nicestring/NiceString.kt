de1afeecd42f71420597557c322e8dbbae0c043c
liyongchen <chenliyo99@gmail.com>
2019-03-25 21:33:50 -0700

    change the variable names

--- Nice String/Task/src/nicestring/NiceString.kt
+++ Nice String/Task/src/nicestring/NiceString.kt
@@ -4,19 +4,19 @@ fun String.isNice(): Boolean {
     var len = this.length
     if (len<2) return false
     var vowelNum = 0
-    var connected = false
-    var hasBX = false
+    var twoContinuous = false
+    var haveBuae = false
     if(this.get(0) in setOf<Char>('a','e','i','o','u')) vowelNum=1
     for(i in 1..len-1){
         var ch=this.get(i)
         if(ch in setOf<Char>('a','e','i','o','u')) vowelNum += 1
-        if(!hasBX&&(ch=='u'||ch=='a'||ch=='e') && (this.get(i-1) =='b'))
-            hasBX=true
-        if(!connected&&ch==this.get(i-1)) connected=true
+        if(!haveBuae&&(ch=='u'||ch=='a'||ch=='e') && (this.get(i-1) =='b'))
+            haveBuae=true
+        if(!twoContinuous&&ch==this.get(i-1)) twoContinuous=true
     }
     var condNum=0
-    if(!hasBX) condNum+=1
-    if(connected) condNum+=1
+    if(!haveBuae) condNum+=1
+    if(twoContinuous) condNum+=1
     if(vowelNum>2) condNum+=1
     return condNum>1
 }
